22c1b01c512973e86093c3321476b69a32b43e53
zied hosni <zied.hosni@wahi.com>
2025-01-01 21:50:33 +0100

    fix: update documentation comment in BluePrint component for clarity

--- src/component/BluePrint/index.tsx
+++ src/component/BluePrint/index.tsx
@@ -5,10 +5,10 @@ interface BluePrintProps {
   subtitle: string;
 }
 /*
-  * BluePrint component is a blue print for a React web app. It is a simple component
-  * that you can copy/paste to create new components by keeping the same structure.
-  * rename / update the component and the story file as needed.
-*/
+ * BluePrint component is a blue print for a React web app. It is a simple component
+ * that you can copy/paste to create new components by keeping the same structure.
+ * rename / update the component and the story file as needed.
+ */
 const BluePrint: React.FC<BluePrintProps> = ({ title, subtitle }) => (
   <div className="bg-blue-500 text-white p-4">
     <h1>{title}</h1>
